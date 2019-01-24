0a77c84eeab1a93df3f9524d0d79f2353022d2a4
ghostxbh <ghostxbh@hotmail.com>
2019-01-24 11:23:52 +0800

    基数排序算法

---
+++ src/com/uzykj/algorithm/radixSort/RadixSort.java
@@ -0,0 +1,66 @@
+package com.uzykj.algorithm.radixSort;
+
+import java.util.ArrayList;
+
+/**
+ * @author ghostxbh
+ * @date 24/01/2019 11:11
+ */
+public class RadixSort {
+
+    /**
+     * @param array
+     * @return
+     * @MethodName 基数排序
+     * @MethodDescribe 是一种非比较型整数排序算法，其原理是将整数按位数切割成不同的数字，然后按每个位数分别比较。
+     * 由于整数也可以表达字符串（比如名字或日期）和特定格式的浮点数，所以基数排序也不是只能使用于整数。
+     */
+    public static int[] radixSort(int[] array) {
+        //首先确定排序的趟数;
+        int max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        int time = 0;
+        //判断位数;
+        while (max > 0) {
+            max /= 10;
+            time++;
+        }
+
+
+        //建立10个队列;
+        ArrayList<ArrayList<Integer>> queue = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            ArrayList<Integer> queue1 = new ArrayList<>();
+            queue.add(queue1);
+        }
+
+
+        //进行time次分配和收集;
+        for (int i = 0; i < time; i++) {
+            //分配数组元素;
+            for (int anArray : array) {
+                //得到数字的第time+1位数;
+                int x = anArray % (int) Math.pow(10, i + 1) / (int) Math.pow(10, i);
+                ArrayList<Integer> queue2 = queue.get(x);
+                queue2.add(anArray);
+                queue.set(x, queue2);
+            }
+            int count = 0;//元素计数器;
+            //收集队列元素;
+            for (int k = 0; k < 10; k++) {
+                while (queue.get(k).size() > 0) {
+                    ArrayList<Integer> queue3 = queue.get(k);
+                    array[count] = queue3.get(0);
+                    queue3.remove(0);
+                    count++;
+                }
+            }
+        }
+        return array;
+    }
+
+}

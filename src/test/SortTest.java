4ff1553cad041e86fab2909c38206e0d3cc91b63
ghostxbh <ghostxbh@hotmail.com>
2021-12-01 21:01:07 +0800

    修改测试区域

--- src/test/SortTest.java
+++ src/test/SortTest.java
@@ -1,11 +1,11 @@
-package Test;
+package test;
 
 
-import com.uzykj.algorithm.exchangeSort.QuickSort;
+import com.uzykj.algorithm.sort.exchangeSort.QuickSort;
 
 import java.util.Arrays;
 
-public class Test1 {
+public class SortTest {
     // 排序原始数据
     private static final int[] NUMBERS =
             {49, 38, 65, 97, 76, 13, 27, 78, 34, 12, 64, 5, 4, 62, 99, 98, 54, 56, 17, 18, 23, 34, 15, 35, 25, 53, 51};
